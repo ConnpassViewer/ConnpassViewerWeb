d3e88f5b4c1a6359651bb942f8c4bdcfb3bd0cc8
daikikojima <daikikojima0531@icloud.com>
2018-07-04 23:54:13 +0900

    ConnpassScraper/ConnpassScraper/spiders/connpass.py: fix bug
    fix the bug that is yielding incorrect value

--- ConnpassScraper/ConnpassScraper/spiders/connpass.py
+++ ConnpassScraper/ConnpassScraper/spiders/connpass.py
@@ -9,11 +9,10 @@ class ConnpassSpider(scrapy.Spider):
     name = 'connpass'
     allowed_domains = ['connpass.com']
     events = requests.get("https://connpass.com/api/v1/event/").json()["events"]
-    event_count = 0
     start_urls = [event["event_url"] for event in events]
     
     def parse(self, response):
-        event = self.events[self.event_count]
+        event = self.events[self.start_urls.index(response.url)]
         item = ConnpassLoader(ConnpassItem(), response=response)
         item.add_xpath('img_url', '//*[@id="main"]/div[1]/div[1]/a/@href')
         item.add_value('event_id', event['event_id'])
@@ -24,4 +23,3 @@ class ConnpassSpider(scrapy.Spider):
         item.add_value('ended_at', event['ended_at'])
         item.add_value('address', event['address'])
         yield item.load_item()
-        self.event_count += 1
